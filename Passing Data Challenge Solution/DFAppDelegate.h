6644b82c7b2c1787d823a8e49b192f31a8be2ec2
dsfank <dsfank@live.com>
2014-07-27 15:15:14 -0500

    Initial Commit

---
+++ Passing Data Challenge Solution/DFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DFAppDelegate.h
+//  Passing Data Challenge Solution
+//
+//  Created by Daren Fankhauser on 7/27/14.
+//  Copyright (c) 2014 Daren Fankhauser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
